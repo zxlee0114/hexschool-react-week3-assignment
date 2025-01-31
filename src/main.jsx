41179c1fa78792a633830c5b67ef234f7107cfa1
zxlee0114 <napoleon.lee0114@gmail.com>
2025-02-01 00:39:19 +0800

    env: 基礎環境建置

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+
+import "./assets/all.scss"
+import "bootstrap/dist/css/bootstrap.css"
+
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
