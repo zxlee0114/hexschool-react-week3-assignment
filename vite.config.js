11e221bae888897424983c915ba6cd90dae740be
zxlee0114 <napoleon.lee0114@gmail.com>
2025-01-31 15:41:50 +0800

    env: 安裝 gh-pages、設置路徑

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/hexschool-react-week3-assignment/" : "/",
   plugins: [react()],
 })
